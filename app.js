72ce4b7d06d95091dea0c9348fc7e641324fc01d
tech-in-tech <anubhavsharma06dec@gmail.com>
2024-02-01 15:22:38 +0530

    80% complete

--- app.js
+++ app.js
@@ -4,9 +4,53 @@ let compScore = 0;
 // selecting choice box;
 
 const choices = document.querySelectorAll(".choice");
+
+const getCompChoice = () => {
+  const opt = ["rock","paper","scissors"];
+  let randomNum = Math.floor(Math.random()*3);
+  return opt[randomNum];
+
+}
+
+const drowGame = ()=>{
+  console.log("!! Drow Game !!");
+}
+const showWinner = (userWin)=>{
+  if(userWin){
+    console.log("!! You Win !!");
+  }
+  else{
+    console.log("!! You Loss !!");
+  }
+}
+const playGame = (userChoice)=>{
+  console.log("user choice is",userChoice)
+  let compChoice = getCompChoice();
+  console.log("comp choice is",compChoice);
+  if(userChoice===compChoice){
+    drowGame();
+  }
+  else{
+    let userWin = true;
+    if(userChoice ==="rock"){
+      userWin = compChoice === "paper" ? false : true;
+    }
+    else if(userChoice ==="paper"){
+      userWin = compChoice==="scissors" ? false : true;
+    }
+    else{
+      userWin = compChoice === "rock" ? false : true ;
+    }
+
+    showWinner(userWin);
+
+  }
+}
+
+//! user choice
 choices.forEach((choice)=>{
   choice.addEventListener("click",()=>{
     const userChoice = choice.getAttribute("id");
-    console.log("Choice was click",userChoice);
+    playGame(userChoice);
   })
 })
